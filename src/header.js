a51eaab38da01d88a5a829704aaa41476d25c91c
keshavsoft <nknnkumar@yahoo.com>
2026-06-06 13:57:49 +0530

    2 icons

--- src/header.js
+++ src/header.js
@@ -1,8 +1,8 @@
 // src/header.js
-import initHeader from "../bin/header/v3/commands/header/template/v5/initHeader.js";
+import initHeader from "../bin/header/v3/commands/header/template/v6/initHeader.js";
 
 (async () => {
-    window.KSHeaderVersion = "v3.5";
+    window.KSHeaderVersion = "v3.6";
 
     window.KSHeader = initHeader;
 })();
